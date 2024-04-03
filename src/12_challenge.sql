e8d9d09e51ef97d72e86b2f466e3e835d40ab580
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-04-03 16:41:54 -0700

    Requisito 16

--- src/12_challenge.sql
+++ src/12_challenge.sql
@@ -1,4 +1,4 @@
 SELECT name AS 'Artista', COUNT(artist_id) AS 'Quantidade de álbuns'
 FROM artists INNER JOIN albums ON artists.id = albums.artist_id
 GROUP BY name
-ORDER BY COUNT(artist.id) DESC, name;
+ORDER BY COUNT(artist_id) DESC, name;
--- src/13_challenge.sql
+++ src/13_challenge.sql
@@ -1,5 +1,5 @@
 SELECT name AS 'Artista'
 FROM artists INNER JOIN albums ON artists.id = albums.artist_id
 GROUP BY name
-HAVING COUNT(album.artist_id) >= 3
+HAVING COUNT(artist_id) > 2
 ORDER BY name;
